a1009a1c3988930f78475acc456e2792b8332c54
pengxt <yyaadet2002@gmail.com>
2015-09-22 23:21:59 +0800

    bug fix

--- clawer/clawer/management/commands/task_analysis.py
+++ clawer/clawer/management/commands/task_analysis.py
@@ -64,7 +64,7 @@ def do_run(clawer_task):
     try:
         out = subprocess.check_output(["/bin/echo", json.dumps({"path":clawer_task.store, "url":clawer_task.uri}), "|", settings.PYTHON, path],
                                       stderr=subprocess.STDOUT)
-        result = json.loads(out)
+        result = json.loads(out.decode("utf-8"))
         result["_url"] = clawer_task.uri
         if clawer_task.cookie:
             result["_cookie"] = clawer_task.cookie
