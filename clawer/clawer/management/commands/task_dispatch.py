76a3a49fe5c5167c6b896548e1f1e0fe23e97434
pengxt <yyaadet2002@gmail.com>
2015-09-23 10:45:43 +0800

    code tuning

--- clawer/clawer/management/commands/task_dispatch.py
+++ clawer/clawer/management/commands/task_dispatch.py
@@ -2,25 +2,18 @@
 
 
 from optparse import make_option
-import sys
-import threading
 
 from django.core.management.base import BaseCommand
 
 from html5helper.utils import wrapper_raven
 from clawer.models import Clawer, ClawerTask
 from clawer.utils import DownloadQueue, download_clawer_task
-from django.conf import settings
 
 
 
 def run(run_time):
     download_queue = DownloadQueue()
     clawers = Clawer.objects.filter(status=Clawer.STATUS_ON).all()
-    
-    #add watcher
-    watcher = threading.Timer(run_time, force_exit)
-    watcher.start()
     
     for clawer in clawers:
         clawer_tasks = ClawerTask.objects.filter(clawer_id=clawer.id, status=ClawerTask.STATUS_LIVE).order_by("id")[:clawer.settings().dispatch]
@@ -31,10 +24,6 @@ def run(run_time):
         
     return download_queue
 
-
-def force_exit():
-    print "force exit dispatch after run"
-    sys.exit(0)
 
 
 class Command(BaseCommand):
@@ -50,5 +39,4 @@ class Command(BaseCommand):
     
     @wrapper_raven
     def handle(self, *args, **options):
-        run_time = int(options["run"])
-        run(run_time)
+        run()
