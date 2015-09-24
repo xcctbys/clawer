200b98f979cc3403f16b5f3f75b8ecc7993ea763
pengxt <yyaadet2002@gmail.com>
2015-09-24 10:27:04 +0800

    bug fix

--- clawer/settings_pro.py
+++ clawer/settings_pro.py
@@ -47,6 +47,7 @@ CELERYD_MAX_TASKS_PER_CHILD = 1024
 CELERY_ACCEPT_CONTENT = ['pickle', 'json', 'msgpack', 'yaml']
 
 REDIS = "redis://10.171.34.147/0"
+URL_REDIS = "redis://10.172.241.155/0"
 
 
 RAVEN_CONFIG = {
