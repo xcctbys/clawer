d52cc1f4dd98ad052342aeb6e5c8f34ce8b0c9e7
pengxt <yyaadet2002@gmail.com>
2015-10-18 19:55:21 +0800

    total 297, success 39, 13.13%

--- sources/qyxy/ocr_train.py
+++ sources/qyxy/ocr_train.py
@@ -1,4 +1,6 @@
 #encoding=utf-8
+""" required: opencv and tesseract
+"""
 
 import urllib
 import json
@@ -21,7 +23,7 @@ from matplotlib import pyplot as plt
 
 
 
-DEBUG = True
+DEBUG = False
 if DEBUG:
     level = logging.DEBUG
 else:
@@ -33,30 +35,34 @@ logging.basicConfig(level=level, format="%(levelname)s %(asctime)s %(lineno)d:: 
 
 
 class Ocr(object):
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, image_path, image_hash):
         self.tesseract = "/usr/local/bin/tesseract"
+        self.chinese = None
+        self.english = None
+        self.image_path = image_path
+        self.image_hash = image_hash
         
     def to_text(self):
-        r = requests.get(self.url)
-        if r.status_code != 200:
-            logging.warn("request %s failed, status code %d", self.url, r.status_code)
-            return None
-        image_id = hashlib.md5(self.url).hexdigest()[-6:]
-        parent = "/Users/pengxt/Documents/ocr"
-        if os.path.exists(parent) is False:
-            os.makedirs(parent, 0775)
-        
-        path = os.path.join(parent, image_id)
-        with open(path, "w") as f:
-            f.write(r.content)
-        
-        for page in range(0, 11): 
-            out_chi = os.path.join(parent, "%s_chi_%d" % (image_id, page))
-            subprocess.call([self.tesseract, path, out_chi, "-l", "chi_sim", "-psm", str(page)])
+        parent = os.path.dirname(__file__)
+        out_chi = os.path.join(parent, "%s_chi" % (self.image_hash))
+        subprocess.call([self.tesseract, self.image_path, out_chi, "-l", "chi_sim", "-psm", "7"])
+        out_chi += ".txt"
+        with open(out_chi, "r") as f:
+            self.chinese = f.read().strip()
+        os.remove(out_chi)
             
-            out_eng = os.path.join(parent, "%s_eng_%d" % (image_id, page))
-            subprocess.call([self.tesseract, path, out_eng, "-l", "eng", "-psm", str(page)])
+        out_eng = os.path.join(parent, "%s_eng" % (self.image_hash))
+        subprocess.call([self.tesseract, self.image_path, out_eng, "-l", "eng", "-psm", "8"])
+        out_eng += ".txt"
+        with open(out_eng, "r") as f:
+            self.english = f.read()
+        os.remove(out_eng)
+        
+        new_english = ""
+        for c in self.english.strip():
+            if c.isdigit() or c.isalpha():
+                new_english += c
+        self.english = new_english
 
 
 class TrainCaptcha(object):
@@ -64,6 +70,7 @@ class TrainCaptcha(object):
         self.save_dir = "train"
         self.image_url = d["image_url"]
         self.image_hash = d["image_hash"]
+        self.labels = d["labels"]
         if os.path.exists(self.save_dir) is False:
             os.makedirs(self.save_dir, 0775)
             
@@ -71,34 +78,67 @@ class TrainCaptcha(object):
         return os.path.join(self.save_dir, self.image_hash)
     
     def gray_path(self):
-        return os.path.join(self.save_dir, self.image_hash, "_gray")
+        return os.path.join(self.save_dir, self.image_hash+"_gray.jpeg")
         
     def filter(self):
         raw_im = cv2.imread(self.raw_path())
         gray_im = cv2.cvtColor(raw_im, cv2.COLOR_BGR2GRAY)
-        mean_thresholding_im = cv2.adaptiveThreshold(gray_im, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 2)
-        blur_im = cv2.medianBlur(mean_thresholding_im, 5)
+        ret, thresholding_im = cv2.threshold(gray_im, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+        #thresholding_im = cv2.adaptiveThreshold(gray_im, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 15, 5)
+        blur_im = cv2.medianBlur(thresholding_im, 5)
         
-        ims = [
-            [raw_im, "raw"],
-            [gray_im, "gray"],
-            [mean_thresholding_im, "mean thresholding"],
-            [blur_im, "blur"],
-        ]
+        """
+        mean_thresholding_im = cv2.adaptiveThreshold(gray_im, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 15, 2)
+        gauss_blur_im = cv2.GaussianBlur(mean_thresholding_im, (5, 5), 0)
+        blur_im = cv2.medianBlur(gauss_blur_im, 5)
+        """
         
-        for i in range(len(ims)):
-            [im, title] = ims[i]
-            plt.subplot(4, 2, i+1)
-            plt.imshow(im, "gray")
-            plt.xticks([])
-            plt.yticks([])
-            plt.title("%s" % (title))
+        cv2.imwrite(self.gray_path(), blur_im)
+        
+        if DEBUG:
+            ims = [
+                [raw_im, "raw"],
+                [gray_im, "gray"],
+                
+                [thresholding_im, "thresholding"],
+                [blur_im, "blur"],
+            ]
             
+            for i in range(len(ims)):
+                [im, title] = ims[i]
+                plt.subplot(4, 2, i+1)
+                plt.imshow(im, "gray")
+                plt.xticks([])
+                plt.yticks([])
+                plt.title("%s" % (title))
             
+            plt.show()
+            plt.close()   
+             
         
-        plt.show()
-        plt.close()        
-
+        
+    def human_label(self):
+        result = None
+        for i in range(len(self.labels)):
+            guess = self.labels[i]
+            for j in range(len(self.labels)):
+                if i == j:
+                    continue
+                if self.labels[j] == guess:
+                    result = guess
+                    break
+        
+        if result:
+            result = result.upper()
+        
+        return result
+    
+    def machine_label(self):
+        self.filter()
+        ocr = Ocr(self.gray_path(), self.image_hash)
+        ocr.to_text()
+        return ocr.english.strip().upper(), ocr.chinese.strip().upper()
+                
 
 class OpencvTrain(object):
     def __init__(self):
@@ -120,6 +160,19 @@ class OpencvTrain(object):
             image_r = requests.get(captcha.image_url)
             with open(save_path, "w") as f:
                 f.write(image_r.content)
+                
+    def train(self):
+        success = 0
+        total = len(self.captchas)
+        for captcha in self.captchas:
+            human_label = captcha.human_label()
+            eng, chi = captcha.machine_label()
+            if eng == human_label:
+                success += 1
+                
+            print u"[sucess %d]: human %s, machine %s" % (success, human_label, unicode(eng, "utf-8"))
+        
+        print "total %d, success %d, %.2f%%" % (total, success, float(success*100)/total)
                 
     
 
@@ -153,4 +206,5 @@ if __name__ == "__main__":
         
     opencv_train = OpencvTrain()
     opencv_train.load_data()
+    opencv_train.train()
         
