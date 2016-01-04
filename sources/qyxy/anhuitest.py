e494fa508e36b6a6af43d2bc5655ad05220f5b2a
haijunt <haijunt@princetechs.com>
2016-01-04 19:59:41 +0800

    haijunt

--- sources/qyxy/anhuitest.py
+++ sources/qyxy/anhuitest.py
@@ -53,9 +53,132 @@ class Anhui(object):
 				if divs == None:continue
 				print divs.ul.li.a['href']
 				resp = self.reqst.get('http://www.ahcredit.gov.cn'+ divs.ul.li.a['href'])
-				print divs.ul.li.a['href'][divs.ul.li.a['href'].find('id=')+3:]
+				mainId = divs.ul.li.a['href'][divs.ul.li.a['href'].find('id=')+3:]
 				if resp.status_code == 200:
 					soup = BeautifulSoup(resp.content,'html5lib')
+					#soup = BeautifulSoup(resp.content,'lxml')
+					mydict={temp['entName']:None}
+					tables = soup.find_all('table')
+					print len(tables)
+					# print len(soup.find_all('div'))
+					# for i, divs in enumerate(soup.body.find_all('div')[8:]):
+					# 	print '.............',i,'................'
+					# 	print divs
+					# 	print '\n\n\n\n\n----------\n\n\n\n\n'
+					for trs in tables[0].find_all('tr'):
+						if trs.td:
+							for ths in trs.find_all('th'):
+								if ths.get_text():
+									print ths.get_text().strip()
+							for tds in trs.find_all('td'):
+								if tds.get_text():
+									print tds.get_text().strip()
+
+					print  tables[1].tr.find('th').get_text().split('\n')[0]
+					for ths in tables[1].find_all('th', attrs={'width':'20%'}):
+						print ths.string	
+					QueryInvList = 'http://www.ahcredit.gov.cn/QueryInvList.jspx?'
+					Host = 'http://www.ahcredit.gov.cn'
+					for i in range(1, len(tables[3].find_all('a'))+1):
+						tempurl =  QueryInvList + 'pno=' + str(i) + '&mainId='+mainId
+						resp = self.reqst.get(tempurl)
+						if resp.status_code == 200:
+							tempsoup = BeautifulSoup(resp.content)
+							for trs in tempsoup.find_all('tr'):
+								for tds in trs.find_all('td'):
+									if tds.find('a'):
+										detail_resp = self.reqst.get(Host + tds.a['onclick'][13:-2])
+										if detail_resp.status_code == 200:
+											detailsoup = BeautifulSoup(detail_resp.content)
+											detail_table = detailsoup.find('table')
+											detailths = detail_table.find_all('th')
+											detailths_yes = detailths[1:4] + detailths[6:]
+											for ths in detailths_yes:
+												print ths.get_text()
+											print '\n'
+											detailtds = detail_table.find_all('td')
+											#detailtds_yes = detailtds[1:4] + detailtds[6:]
+											for tds in detailtds:
+												print tds.get_text()
+											
+									if tds.string:
+										print tds.string.strip()
+
+					for ths in tables[4].find_all('th'):
+						print ths.get_text()
+					for trs in tables[5].find_all('tr'):
+						for tds in trs.find_all('td'):
+							print tds.get_text()
+
+					for ths in tables[6].find_all('th')[1:4]:
+						print ths.string
+					QueryMemList = 'http://www.ahcredit.gov.cn/QueryMemList.jspx?'
+					for i in range(1, len(tables[8].find_all('a'))+1):
+						tempurl =  QueryMemList + 'pno=' + str(i) + '&mainId='+mainId
+						resp = self.reqst.get(tempurl)
+						if resp.status_code == 200:
+							tempsoup = BeautifulSoup(resp.content)
+							for tds in tempsoup.find_all('td'):
+								print tds.string
+
+					for ths in tables[9].find_all('th')[1:5]:
+						print ths.string
+					QueryChildList = 'http://www.ahcredit.gov.cn/QueryChildList.jspx?'
+					for i in range(1, len(tables[11].find_all('a'))+1):
+						tempurl =  QueryChildList + 'pno=' + str(i) + '&mainId='+mainId
+						resp = self.reqst.get(tempurl)
+						if resp.status_code == 200:
+							tempsoup = BeautifulSoup(resp.content)
+							for tds in tempsoup.find_all('td'):
+								print tds.string
+					
+					for trs in tables[12].find_all('tr')[1:]:
+						print trs.th.string + ':' + trs.td.string
+
+					for ths in tables[16].find_all('th')[1:]:
+						print ths.string
+					for trs in tables[17].find_all('tr'):
+						for tds in trs.find_all('td'):
+							print tds.string
+
+					for ths in tables[13].find_all('th')[1:]:
+						print ths.string
+					QueryPledgeList = 'http://www.ahcredit.gov.cn/QueryPledgeList.jspx?'
+					for i in range(1, len(tables[15].find_all('a'))+1):
+						tempurl =  QueryPledgeList + 'pno=' + str(i) + '&mainId='+mainId
+						resp = self.reqst.get(tempurl)
+						if resp.status_code == 200:
+							tempsoup = BeautifulSoup(resp.content)
+							for tds in tempsoup.find_all('td'):
+								print tds.string
+
+
+					for ths in tables[22].find_all('th')[1:]:
+						print ''.join(ths.get_text())
+					for trs in tables[23].find_all('tr'):
+						for tds in trs.find_all('td'):
+							print tds.string
+
+					for ths in tables[18].find_all('th')[1:]:
+						print ths.string
+					for trs in tables[19].find_all('tr'):
+						for tds in trs.find_all('td'):
+							print tds.string
+
+					for ths in tables[20].find_all('th')[1:]:
+						print ths.string
+					for trs in tables[21].find_all('tr'):
+						for tds in trs.find_all('td'):
+							print tds.string
+
+					for ths in tables[24].find_all('th')[1:]:
+						print ths.string
+					for trs in tables[25].find_all('tr'):
+						for tds in trs.find_all('td'):
+							print tds.string
+
+					
+
 					table1 = soup.find(class_='detailsList')
 					f = open('table1.txt', 'w')
 					for index, tr in enumerate(table1.find_all('tr')):
@@ -81,19 +204,20 @@ class Anhui(object):
 						for item in table2:
 							if item.a:
 								urlid = item.a['onclick'][item.a['onclick'].find('window.open')+13:-2]
-								print 'http://www.ahcredit.gov.cn'+urlid
-								print item.get_text().strip()
+								#print 'http://www.ahcredit.gov.cn'+urlid
+								#print item.get_text().strip()
 								resp = self.reqst.get('http://www.ahcredit.gov.cn'+urlid)
 								tempsoup = BeautifulSoup(resp.content,'html5lib')
 								tabeldetail = tempsoup.find('table',attrs={'cellpadding':"0" ,'cellspacing':"0", 'class':"detailsList" })
 								ths = tabeldetail.find_all('th')
 								tds = tabeldetail.find_all('tr')
-								for th in ths:
-									print th.get_text().strip()
-								for td in tds:
-									print td.get_text().strip()
+								# for th in ths:
+								# 	print th.get_text().strip()
+								# for td in tds:
+								# 	print td.get_text().strip()
 							else :
-								print item.get_text().strip()
+								pass
+								#print item.get_text().strip()
 						return True
 					else :
 						print 'error...if'
