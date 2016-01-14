03f982ef606fd8732b066e5659338ffcc32d3b0f
zhongyid <zhongyid@princetechs.com>
2016-01-14 14:53:56 +0800

    add tables class

--- sources/qyxy/structured/clawer_parse/models.py
+++ sources/qyxy/structured/clawer_parse/models.py
@@ -133,3 +133,100 @@ class industry_commerce_mortgage(models.Model):
     details = models.TextField()
     enter_id = models.CharField(max_length=20)
     bas_id = models.IntegerField()
+
+
+class industry_commerce_mortgage_detail_change(models.Model):
+    """工商-抵押-详情-变更
+    """
+
+    modify_date = models.DateTimeField()
+    modify_content = models.TextField()
+    register_id = models.CharField(max_length=20)
+    ind_id = models.IntegerField()
+
+
+class industry_commerce_mortgage_detail_guarantee(models.Model):
+    """工商-抵押-详情-抵押权人
+    """
+
+    check_type = models.CharField(max_length=20)
+    amount = models.FloatField()
+    guarantee_scope = models.CharField(max_length=100)
+    debtor_duration = models.CharField(max_length=20)
+    comment = models.CharField(max_length=100)
+    register_id = models.CharField(max_length=20)
+    ind_id = models.IntegerField()
+
+
+class industry_commerce_mortgage_guaranty(models.Model):
+    """工商-抵押-详情-抵押物
+    """
+
+    name = models.CharField(max_length=30)
+    ownership = models.CharField(max_length=30)
+    details = models.TextField()
+    coments = models.CharField(max_length=100)
+    register_id = models.CharField(max_length=20)
+    ind_id = models.IntegerField()
+
+
+class industry_commerce_revoke(models.Model):
+    """工商-撤销
+    """
+
+    revoke_item = models.CharField(max_length=30)
+    content_before_revoke = models.CharField(max_length=50)
+    content_after_revoke = models.CharField(max_length=50)
+    revoke_date = models.DateTimeField()
+    enter_id = models.CharField(max_length=20)
+    bas_id = models.IntegerField()
+
+class industry_commerce_shareholders(models.Model):
+    """工商-股东
+    """
+
+    shareholder_type = models.CharField(max_length=20)
+    shareholder_name = models.CharField(max_length=30)
+    certificate_type = models.CharField(max_length=20)
+    certificate_number = models.CharField(max_length=20)
+    subscription_amount = models.FloatField()
+    paid_amount = models.FloatField()
+    subscription_type = models.CharField(max_length=30)
+    subscription_date = models.DateTimeField()
+    subscription_money_amount = models.FloatField()
+    paid_type = models.CharField(max_length=20)
+    paid_money_amount = models.FloatField()
+    paid_date = models.DateTimeField()
+    enter_id = models.CharField(max_length=20)
+    bas_id = models.IntegerField()
+
+
+class industry_commerce_sharepledge(models.Model):
+    """
+    """
+
+    register_num = models.CharField(max_length=20)
+    pledgor = models.CharField(max_length=30)
+    pledgor_certificate_code = models.CharField(max_length=20)
+    share_pledge_num = models.FloatField()
+    mortgagee = models.CharField(max_length=30)
+    mortgagee_certificate_code = models.CharField(max_length=20)
+    sharechange_register_date = models.DateTimeField()
+    status = models.CharField(max_length=20)
+    change_detail = models.CharField(max_length=100)
+    publicity_time = models.DateTimeField()
+    modify_date = models.DateTimeField()
+    modify_content = models.TextField()
+    register_id = models.CharField(max_length=20)
+    bas_id = models.IntegerField()
+
+
+class industry_mortgage_detail_mortgagee(models.Model):
+    """工商-抵押-详情-抵押权人
+    """
+
+    mortgagee_name = models.CharField(max_length=30)
+    mortgagee_certificate_type = models.CharField(max_length=20)
+    pledgor_certificate_code = models.CharField(max_length=20)
+    register_id = models.CharField(max_length=20)
+    ind_id = models.IntegerField()
