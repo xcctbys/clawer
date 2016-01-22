6a8e9f4dd640c992e73e3724db852fc7ae0e8135
dawuw <dawuw@princetechs.com>
2016-01-22 17:13:33 +0800

    update run.py

--- sources/qyxy/run.py
+++ sources/qyxy/run.py
@@ -38,7 +38,7 @@ from anhui_crawler import AnhuiCrawler
 from yunnan_crawler import YunnanCrawler
 from tianjin_crawler import TianjinCrawler
 from hunan_crawler import HunanCrawler
-from fujian_crawler import FujianCrawler
+# from fujian_crawler import FujianCrawler
 from sichuan_crawler import SichuanCrawler
 from shandong_crawler import ShandongCrawler
 from hebei_crawler import HebeiCrawler
@@ -46,7 +46,7 @@ from shaanxi_crawler import ShaanxiCrawler
 from henan_crawler import HenanCrawler
 from neimenggu_crawler import NeimengguClawer
 from chongqing_crawler import ChongqingClawer
-#from xinjiang_crawler import XinjiangClawer
+from xinjiang_crawler import XinjiangClawer
 from zhejiang_crawler import ZhejiangCrawler
 from liaoning_crawler import LiaoningCrawler
 
@@ -62,7 +62,7 @@ province_crawler = {
     'yunnan':YunnanCrawler,
     'tianjin' : TianjinCrawler,
     'hunan' : HunanCrawler,
-    'fujian' : FujianCrawler,
+    # 'fujian' : FujianCrawler,
     'sichuan': SichuanCrawler,
     'shandong' : ShandongCrawler,
     'hebei' : HebeiCrawler,
