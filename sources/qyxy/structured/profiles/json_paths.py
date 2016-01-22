1c826390fd91c38da12fc61c4e5d7c66a9771ef1
zhongyid <zhongyid@princetechs.com>
2016-01-22 14:36:23 +0800

    v0.8

---
+++ sources/qyxy/structured/profiles/json_paths.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+json_paths = (
+    "profiles/jsons/beijing.json",
+    "profiles/jsons/hebei.json",
+    "profiles/jsons/henan.json",
+    "profiles/jsons/heilongjiang.json",
+    "profiles/jsons/tianjin.json",
+    "profiles/jsons/zhejiang.json",
+    "profiles/jsons/neimeng.json",
+)
+
+"profiles/jsons/anhui.json",
+"profiles/jsons/fujian.json",
+"profiles/jsons/guangdong.json",
+"profiles/jsons/hunan.json",
+"profiles/jsons/jiangsu.json",
+"profiles/jsons/shandong.json",
+"profiles/jsons/shanghai.json",
+"profiles/jsons/sichuan.json",
+"profiles/jsons/yunan.json",
+"profiles/jsons/zongju.json",
+"profiles/jsons/gansu.json",
+
+"profiles/jsons/liaoning.json",
+"profiles/jsons/shaanxi.json",
+
+"profiles/jsons/beijing.json",
+"profiles/jsons/hebei.json",
+"profiles/jsons/henan.json",
+"profiles/jsons/heilongjiang.json",
+"profiles/jsons/tianjin.json",
+"profiles/jsons/zhejiang.json",
+"profiles/jsons/neimeng.json",
