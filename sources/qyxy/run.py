b630997ff9c58398b80058740940cf0a392f58b3
pengxt <yyaadet2002@gmail.com>
2016-01-22 11:26:09 +0800

    use process pool

--- sources/qyxy/run.py
+++ sources/qyxy/run.py
@@ -48,7 +48,7 @@ from chongqing_crawler import ChongqingClawer
 from zhejiang_crawler import ZhejiangCrawler
 from liaoning_crawler import LiaoningCrawler
 
-failed_ent = {}
+
 province_crawler = {
     'beijing': BeijingCrawler,
     'jiangsu': JiangsuCrawler,
@@ -73,7 +73,8 @@ province_crawler = {
     'liaoning': LiaoningCrawler
 }
 
-max_crawl_time = 0
+process_pool = multiprocessing.Pool(processes=4)
+cur_date = CrawlerUtils.get_cur_y_m_d()
 
 
 def set_codecracker():
@@ -119,7 +120,7 @@ def crawl_work(n, province, json_restore_path, ent_queue):
             ent_queue.task_done()
 
 
-def crawl_province(province, cur_date):
+def crawl_province(province):
     #创建存储路径
     json_restore_dir = '%s/%s/%s/%s' % (settings.json_restore_path, province, cur_date[0], cur_date[1])
     if not os.path.exists(json_restore_dir):
@@ -165,6 +166,7 @@ def crawl_province(province, cur_date):
 
 def force_exit():
     settings.logger.error("run timeout")
+    process_pool.terminate()
     os._exit(1)
 
 
@@ -257,30 +259,25 @@ def main():
 
     settings.logger.info(u'即将开始爬取，最长爬取时间为 %s 秒' % settings.max_crawl_time)
     settings.start_crawl_time = datetime.datetime.now()
-
+    
+    args = []
     if sys.argv[2] == 'all':
-        for p in sorted(province_crawler.keys()):
-            process = multiprocessing.Process(target=crawl_province, args=(p, cur_date))
-            process.daemon = True
-            process.start()
-            process.join(max_crawl_time/len(province_crawler.keys()))
+        args = [p for p in sorted(province_crawler.keys())]
     else:
         provinces = sys.argv[2:]
         for p in provinces:
             if not p in province_crawler.keys():
                 settings.logger.warn('province %s is not supported currently' % p)
             else:
-                process = multiprocessing.Process(target=crawl_province, args=(p, cur_date))
-                process.daemon = True
-                process.start()
-                process.join(max_crawl_time)
-                settings.logger.info("child process exit code %d", process.exitcode)
+                args.append(p)
+    
+    process_pool.map(crawl_province, args)
+    process_pool.join()
     
     
 def send_sentry_report():
     if settings.sentry_client:
         settings.sentry_client.captureException()
-
 
 
 if __name__ == '__main__':
