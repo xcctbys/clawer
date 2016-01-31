eccc17ccc56d44e8e869dd5cee42bedc02660ff0
zhongyid <zhongyid@princetechs.com>
2016-01-31 18:26:11 +0800

    read jsons from url

--- sources/qyxy/structured/clawer_parse/management/commands/structured.py
+++ sources/qyxy/structured/clawer_parse/management/commands/structured.py
@@ -2,8 +2,13 @@
 
 from django.core.management.base import BaseCommand
 from clawer_parse.parse import Parse
-from profiles.json_paths import json_paths
 from multiprocessing import Pool
+from profiles import consts
+from profiles import consts
+from datetime import date, timedelta
+from cStringIO import StringIO
+import gzip
+import requests
 import time
 
 
@@ -12,9 +17,20 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         begin = time.time()
         p = Pool(processes=16)
+        base_url = consts.JSONS_URL
+        provinces = consts.PROVINCES
+        yesterday = date.today() - timedelta(1)
+        yesterday_str = yesterday.strftime("%Y/%m/%d")
+        suffix = ".json.gz"
 
-        for json_path in json_paths:
-            p.apply_async(parse, args=(json_path,))
+        for prinvince in provinces:
+            url = base_url + "/" + prinvince + "/" + yesterday_str + suffix
+            response = requests.get(url)
+
+            if int(response.status_code) == 200:
+                gz = gzip.GzipFile(fileobj=StringIO(response.content))
+                companies = gz.readlines()
+                p.apply_async(parse, args=(companies,))
 
         p.close()
         p.join()
@@ -23,10 +39,9 @@ class Command(BaseCommand):
         secs = int(round(end - begin))
         m, s = divmod(secs, 60)
         h, m = divmod(m, 60)
-
         print "✅  Done！耗时 %d时%02d分%02d秒 ✅ " % (h, m, s)
 
 
-def parse(json_path):
-    worker = Parse(json_path)
+def parse(companies):
+    worker = Parse(companies)
     worker.parse_companies()
