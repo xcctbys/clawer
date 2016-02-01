84269d37551f2d95b1f2adcfe66a5ab11b4cd629
pengxt <yyaadet2002@gmail.com>
2016-02-01 11:05:20 +0800

    tuning settings import method

--- sources/qyxy/run.py
+++ sources/qyxy/run.py
@@ -19,13 +19,15 @@ import raven
 from encodings import zlib_codec
 import zlib
 import jinja2
+import importlib
 
 
-ENT_CRAWLER_SETTINGS=os.getenv('ENT_CRAWLER_SETTINGS')
-if ENT_CRAWLER_SETTINGS and ENT_CRAWLER_SETTINGS.find('settings_pro') >= 0:
-    import settings_pro as settings
+ENT_CRAWLER_SETTINGS = os.getenv('ENT_CRAWLER_SETTINGS')
+if ENT_CRAWLER_SETTINGS:
+    settings = importlib.import_module(ENT_CRAWLER_SETTINGS)
 else:
     import settings
+    
 
 from mail import SendMail
 
