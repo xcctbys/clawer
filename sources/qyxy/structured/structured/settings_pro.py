8c46215e3ec4d7ac96f1c4427725b07ad6ee31bf
zhongyid <zhongyid@princetechs.com>
2016-02-01 15:58:38 +0800

    production

--- sources/qyxy/structured/structured/settings_pro.py
+++ sources/qyxy/structured/structured/settings_pro.py
@@ -3,18 +3,18 @@ from settings import *
 import logging
 
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'clawer',
-#        'USER': 'cacti',
-#        'PASSWORD': 'cacti',
-#        'HOST': '10.100.80.50',
-#        'PORT': '3306',
-#    }
-#}
-#
-#JSONS_URL = 'http://10.100.90.51:8080/media/clawer_result/enterprise/json'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'clawer',
+        'USER': 'cacti',
+        'PASSWORD': 'cacti',
+        'HOST': '10.100.80.50',
+        'PORT': '3306',
+    }
+}
+
+JSONS_URL = 'http://10.100.90.51:8080/media/clawer_result/enterprise/json'
 
 LOG_LEVEL = logging.ERROR
 LOG_FORMAT = '%(asctime)s %(name)s %(levelname)s %(pathname)s:%(lineno)d:: %(message)s'
