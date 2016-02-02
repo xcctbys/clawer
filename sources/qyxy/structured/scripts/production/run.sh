775b31c58d43ad8b0660a9cfcf3b467788a9139d
pengxt <yyaadet2002@gmail.com>
2016-02-02 10:30:52 +0800

    change password

--- sources/qyxy/structured/scripts/production/run.sh
+++ sources/qyxy/structured/scripts/production/run.sh
@@ -1,5 +1,16 @@
-PROJECT=~/projects/nice-clawer/sources/qyxy/structured
-PYTHON_BIN=/usr/local/bin/python
+WORKDIR=/home/webapps/nice-clawer/sources/qyxy/structured
+PYTHON=/home/virtualenvs/dj18/bin/python
 
-cd $PROJECT
-$PYTHON_BIN manage_pro.py structured
+
+function safe_run()
+{
+    file="/tmp/structure.lock"
+
+    (
+        flock -xn -w 10 200 || exit 1
+        cd ${WORKDIR}; ${PYTHON} manage_pro.py structured
+    ) 200>${file}
+}
+
+time safe_run
+
