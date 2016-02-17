a4d61c807b30b0feb7aafc846a2f2a90ed6dde1a
pengxt <yyaadet2002@gmail.com>
2016-02-17 14:19:10 +0800

    remove html path

--- clawer/enterprise/libs/settings.py
+++ clawer/enterprise/libs/settings.py
@@ -2,14 +2,6 @@
 from django.conf import settings
 
 
-#whether to save html page
-save_html = False
-
-html_restore_path = './enterprise_crawler'
-if hasattr(settings, 'ENTERPRISE_HTML_RESTORE_PATH'):
-    html_restore_path = settings.ENTERPRISE_HTML_RESTORE_PATH
-    
-
 json_restore_path = './enterprise_crawler'
 if hasattr(settings, 'ENTERPRISE_JSON_RESTORE_PATH'):
     json_restore_path = settings.ENTERPRISE_JSON_RESTORE_PATH
