24b607669e1fb93602278be4737d25e5c414c3bf
haijunt <haijunt@princetechs.com>
2016-03-01 13:45:02 +0800

    haijunt

--- clawer/enterprise/alanysis_json.py
+++ clawer/enterprise/alanysis_json.py
@@ -203,11 +203,11 @@ db_down_dict = dict([('10',set()),
                 ('91',set())])
 
 
-reqst = requests.Session()
-reqst.headers.update({'Accept': 'text/html, application/xhtml+xml, */*',
-			'Accept-Encoding': 'gzip, deflate',
-			'Accept-Language': 'en-US, en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3',
-			'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:39.0) Gecko/20100101 Firefox/39.0'})
+# reqst = requests.Session()
+# reqst.headers.update({'Accept': 'text/html, application/xhtml+xml, */*',
+# 			'Accept-Encoding': 'gzip, deflate',
+# 			'Accept-Language': 'en-US, en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3',
+# 			'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:39.0) Gecko/20100101 Firefox/39.0'})
 
 def dowload_json_by_days(url):
 	# resp = None
