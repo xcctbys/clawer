26ae10d24727083976a92aa28f5a1257df19c2ee
iygnohz <iygnohz@gmail.com>
2016-03-04 17:09:36 +0800

    Test models

--- clawer/enterprise/migrations/0001_initial.py
+++ clawer/enterprise/migrations/0001_initial.py
@@ -8,20 +8,11 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding model 'Province'
-        db.create_table('enterprise_province', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=128)),
-            ('label', self.gf('django.db.models.fields.CharField')(max_length=128)),
-            ('add_datetime', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-        ))
-        db.send_create_signal('enterprise', ['Province'])
-
         # Adding model 'Enterprise'
         db.create_table('enterprise_enterprise', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=256)),
-            ('province', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['enterprise.Province'])),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=128)),
+            ('province', self.gf('django.db.models.fields.IntegerField')(max_length=128)),
             ('register_no', self.gf('django.db.models.fields.CharField')(max_length=128)),
             ('add_datetime', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
         ))
@@ -29,9 +20,6 @@ class Migration(SchemaMigration):
 
 
     def backwards(self, orm):
-        # Deleting model 'Province'
-        db.delete_table('enterprise_province')
-
         # Deleting model 'Enterprise'
         db.delete_table('enterprise_enterprise')
 
@@ -41,16 +29,9 @@ class Migration(SchemaMigration):
             'Meta': {'object_name': 'Enterprise'},
             'add_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
-            'province': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['enterprise.Province']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'province': ('django.db.models.fields.IntegerField', [], {'max_length': '128'}),
             'register_no': ('django.db.models.fields.CharField', [], {'max_length': '128'})
-        },
-        'enterprise.province': {
-            'Meta': {'object_name': 'Province'},
-            'add_datetime': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'label': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'})
         }
     }
 
